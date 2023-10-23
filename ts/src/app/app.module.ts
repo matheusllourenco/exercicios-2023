ea0da80911253dcc9e0bb1a35c38edcf5c4c5ded
matheusllourenco <matheuslimalourenco@gmail.com>
2023-10-23 04:03:54 -0300

    implementando parte de discussoes e form para criar topico

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SidemenuComponent } from './components/sidemenu/sidemenu.component';
 import { headerComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { ConteudoComponent } from './components/conteudo/conteudo.component';
 import { discussaoComponent } from './components/conteudo/discussao/discussao.component';
 import { resumoComponent} from './components/conteudo/resumo/resumo.component';
+import { CardComponent } from './components/conteudo/discussao/card/card.component';
+import { FooterComponent } from './components/conteudo/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,9 +20,11 @@ import { resumoComponent} from './components/conteudo/resumo/resumo.component';
     ConteudoComponent,
     discussaoComponent,
     resumoComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
